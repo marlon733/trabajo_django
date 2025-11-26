45caef0dedd7749327f5195be8268aa79e768adf
marlon733 <rodriguez.gaitan33@gmail.com>
2025-11-26 06:39:32 -0500

    se guardan archivos

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
   mymembers = Member.objects.all().values()
@@ -20,3 +21,20 @@ def details(request, id):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing(request):
+  template = loader.get_template('template.html')
+  mymembers = Member.objects.all().values()
+  column_firstname = Member.objects.values_list("firstname")
+  records_Carlos = Member.objects.filter(firstname__exact="Carlos").values()
+  colum_range = Member.objects.filter(id__range=(2, 4)).values()
+  busquer_id = Member.objects.filter(id__lte=3).values()
+  context = {
+    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mymembers,
+    'column_firstname': column_firstname,
+    'records_Felipe': records_Carlos,
+    'colum_range': colum_range,
+    'busquer_id': busquer_id,
+  }
+  return HttpResponse(template.render(context, request))
